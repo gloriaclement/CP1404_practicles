12160c54d5b016ae89ad580eb88e413af36b62f4
gloriaclement <gloria.clement@my.jcu.edu.au>
2023-06-16 01:47:55 +1000

    Add scores menu program

---
+++ Prac_02/score_menu.py
@@ -0,0 +1,47 @@
+"""
+CP1404/CP5632 - Practical 02
+Score menu program
+ """
+
+MENU = """G - Get score
+P - Print result
+S - Show score as stars
+Q - Quit"""
+
+
+def main():
+    score = ""
+    print(MENU)
+    choice = input("Choice: ").upper()
+    while choice != 'Q':
+        if choice == 'G':
+            score = get_valid_score()
+        elif choice == 'P':
+            print_grades(score)
+        elif choice == 'S':
+            print("*" * score)
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input("Choice: ")
+    print("Farewell")
+
+
+def print_grades(score):
+    if score < 50:
+        print("Bad")
+    elif score <= 89:
+        print("Passable")
+    else:
+        print("Excellent")
+
+
+def get_valid_score():
+    score = int(input("Score: "))
+    while score < 0 or score > 100:
+        print("Invalid input")
+        score = int(input("Score: "))
+    return score
+
+
+main()
